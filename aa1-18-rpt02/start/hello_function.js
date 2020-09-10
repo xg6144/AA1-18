cefffc13da4a60d0175244d1d82fecbe60f8d11c
JEONDONGBEEN <48852375+xg6144@users.noreply.github.com>
2020-09-10 16:26:03 +0900

    Add files via upload

---
+++ aa1-18-rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what) {
+  console.log("Hello " + what + "!");
+}
+
+hello("aa18");
+hello("xg6144, 전동빈");
---
+++ aa1-18-rpt02/start/hello_my_module.js
@@ -0,0 +1,4 @@
+var olleh = require("./hello_module.js");
+
+olleh("aa18");
+olleh("Node");
